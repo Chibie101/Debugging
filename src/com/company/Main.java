9d5c26289f89248995bb834895747513b33d5af5
Chibuzo Obiefune <chibuzoobiefune@Chibuzos-MacBook-Pro.local>
2022-02-08 17:32:04 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+    }
